02b4048953afcae8aeb00307ec52caba752c4b07
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-12-12 18:14:03 +0530

    css reset colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-cyan': '#2BD0D0',
+        'light-gray': '#EFF1F7',
+        'dark-gray': '#9E9AA8',
+        'dark-violet': '#4B3F6B',
+        'light-violet': '#3A3054',
+        'primary-black': '#34313D',
+      },
+    },
   },
   plugins: [],
 };
