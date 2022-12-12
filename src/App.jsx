e54adb1d07644d9497028e5771ecf0d1796bd194
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-12-12 22:09:39 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import { useState } from 'react';
 import './App.css';
+import Heading from './components/Texts/Heading';
+import Paragraph from './components/Texts/Paragraph';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="text-3xl font-bold underline hover:bg-green-300">
-      Hello world
+    <div>
+      <div className="mb-10">
+        <Heading type="primary">More than just shorter links</Heading>
+      </div>
+      <div className="mb-10">
+        <Heading type="secondary">Advanced Statistics</Heading>
+      </div>
+      <div className="mb-10">
+        <Heading type="smaller">Brand Recognition</Heading>
+      </div>
+      <div className="mb-10">
+        <Paragraph type="primary">
+          Build your brand's recognition and get detailed insights on how your
+          links are performing.
+        </Paragraph>
+      </div>
+      <div className="mb-10">
+        <Paragraph type="secondary">
+          Track how your links are performing across the web with our advanced
+          statistics dashboard.
+        </Paragraph>
+      </div>
+      <div>
+        <Paragraph type="otherType">
+          Boost your brand recognition with each click. Generic links don't mean
+          a thing. Branded links help instil confidence in your content.
+        </Paragraph>
+      </div>
     </div>
   );
 }
