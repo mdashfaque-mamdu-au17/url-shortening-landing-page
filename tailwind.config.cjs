fe10228d1b7146ee465d7c86a6f73b4c1f4959bb
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-12-13 14:14:16 +0530

    color changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,10 @@ module.exports = {
     extend: {
       colors: {
         'dark-cyan': '#2BD0D0',
+        'dark-cyan-hover': '#9AE3E3',
         'light-gray': '#EFF1F7',
         'dark-gray': '#9E9AA8',
+        'medium-gray': '#BFBFBF',
         'dark-violet': '#4B3F6B',
         'light-violet': '#3A3054',
         'primary-black': '#34313D',
