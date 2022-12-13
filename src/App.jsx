8585d4f76adec7cc5b2c213aeab284800b774c15
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-12-13 14:14:29 +0530

    testing  components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { useState } from 'react';
 import './App.css';
+import Button from './components/Button/Button';
+import Link from './components/Button/Link';
 import Heading from './components/Texts/Heading';
 import Paragraph from './components/Texts/Paragraph';
-
+import {
+  Twitter,
+  Facebook,
+  Pinterest,
+} from './components/SocialIcons/SocialIcon';
 function App() {
   const [count, setCount] = useState(0);
 
@@ -29,11 +35,60 @@ function App() {
           statistics dashboard.
         </Paragraph>
       </div>
-      <div>
+      <div className="mb-10">
         <Paragraph type="otherType">
           Boost your brand recognition with each click. Generic links don't mean
           a thing. Branded links help instil confidence in your content.
         </Paragraph>
+      </div>
+      <div className="text-center mb-10">
+        <Button type="rounded" size="medium">
+          Get started
+        </Button>
+      </div>
+      <div className="text-center mb-10">
+        <Button type="rounded" size="small">
+          Sign up
+        </Button>
+      </div>
+      <div className="mb-10 text-center">
+        <Button type="rounded" size="large">
+          Sign up
+        </Button>
+      </div>
+      <div className="text-center mb-10">
+        <Button type="square" size="large">
+          Shorten it
+        </Button>
+      </div>
+      <div className="text-center mb-10">
+        <Button type="square" size="small" isCopied={true}>
+          Shorten it
+        </Button>
+      </div>
+
+      <div className="text-center mb-10">
+        <div className="mb-10">
+          <Link type="web-navigation">Pricing</Link>
+        </div>
+        <div className="h-10 bg-violet-900 mb-10">
+          <Link type="mob-navigation">Features</Link>
+        </div>
+
+        <div className="h-10 bg-primary-black">
+          <div className="mb-10">
+            <Link type="footer-navigation">About</Link>
+          </div>
+          <div className="h-10 bg-primary-black">
+            <Link type="footer-navigation">
+              <Twitter />
+            </Link>
+
+            <Link type="footer-navigation">
+              <Pinterest />
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
