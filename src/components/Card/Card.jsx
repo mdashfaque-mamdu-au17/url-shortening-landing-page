c4604f5b9afe515b03be9be5b4c50fd2ca020444
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-12-19 22:38:26 +0530

    modified it to pass props from outside

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,21 +2,19 @@ import React from 'react';
 import Heading from '../Texts/Heading';
 import Paragraph from '../Texts/Paragraph';
 import IconBrandRecognition from '../../assets/icon-brand-recognition.svg';
-const Card = () => {
+const Card = (props) => {
+  const { title, information, picture } = props;
   return (
-    <div className="bg-white rounded-[5px] px-8 w-[327px] text-center lg:text-left lg:w-[350px] relative">
+    <div className="bg-white rounded-[5px] px-8  text-center lg:text-left w-full relative">
       <div className="pt-[77px] pb-3">
-        <Heading type="smaller">Brand Recognition</Heading>
+        <Heading type="smaller">{title}</Heading>
       </div>
       <div className="pb-[41px]">
-        <Paragraph type="otherType">
-          Boost your brand recognition with each click. Generic links don't mean
-          a thing. Branded links help instil confidence in your content.
-        </Paragraph>
+        <Paragraph type="otherType">{information}</Paragraph>
       </div>
 
       <div className="h-[88px] w-[88px] bg-light-violet rounded-full absolute flex items-center justify-center -top-10 left-1/2 transform -translate-x-1/2 lg:left-8 lg:translate-x-0">
-        <img src={IconBrandRecognition} alt="" />
+        <img src={picture} alt="" />
       </div>
     </div>
   );
