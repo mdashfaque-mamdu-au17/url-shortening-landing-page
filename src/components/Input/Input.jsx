4e93cc7643f77ef8e7fcaf437f8b69780d1fe8b6
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-12-19 11:17:57 +0530

    absolute position for error message on large screens

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -15,13 +15,13 @@ const Input = (props) => {
         placeholder={label}
         onChange={onChange}
         className={classNames(
-          'w-full h-12 lg:h-16 pl-4 text-primary-black text-base lg:text-xl font-medium leading-9 tracking-[0.12px] lg:pl-8 rounded-[5px] md:rounded-[10px] focus:outline-none focus:border-dark-cyan focus:ring-dark-cyan focus:ring-[3px] placeholder:text-primary-black placeholder:text-opacity-50',
+          'w-full h-12 md:h-16 pl-4 text-primary-black text-base lg:text-xl font-medium leading-9 tracking-[0.12px] lg:pl-8 rounded-[5px] md:rounded-[10px] focus:outline-none focus:border-dark-cyan focus:ring-dark-cyan focus:ring-[3px] placeholder:text-primary-black placeholder:text-opacity-50',
           error &&
             'ring-[3px] ring-primary-red placeholder:text-primary-red focus:ring-primary-red'
         )}
       />
       {error && (
-        <div className="text-red-900 text-left italic text-sm md:text-base mt-1 md:mt-2 leading-[18px]">
+        <div className="text-red-900 text-left italic text-sm md:text-base mt-1 md:mt-2 leading-[18px] md:absolute">
           {errorMessage}
         </div>
       )}
