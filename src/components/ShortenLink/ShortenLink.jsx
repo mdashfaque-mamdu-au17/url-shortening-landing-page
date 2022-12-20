76e374532f650ae1ed2140fbe24aa9534cb98050
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-12-20 17:46:50 +0530

    props passed and copy to clipboard functionality added

--- src/components/ShortenLink/ShortenLink.jsx
+++ src/components/ShortenLink/ShortenLink.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import Button from '../Button/Button';
-const ShortenLink = () => {
+const ShortenLink = ({ shortLink, originalLink }) => {
   const [isCopied, setIsCopied] = useState(false);
   const linkCopyHandler = () => {
     setIsCopied(true);
+    navigator.clipboard.writeText(shortLink);
   };
   useEffect(() => {
     let id;
@@ -19,15 +20,15 @@ const ShortenLink = () => {
   return (
     <div className="bg-white rounded-[5px] lg:pl-8 lg:pr-6 lg:py-[18px]">
       <div className="lg:flex lg:items-center lg:justify-between">
-        <div>
-          <p className="text-base font-medium text-primary-black leading-9 py-[6px] px-4 lg:py-0 lg:px-0 lg:text-xl">
-            https://www.frontendmentor.io
+        <div className="lg:max-w-[500px]">
+          <p className="text-base font-medium text-primary-black leading-9 py-[6px] px-4 lg:py-0 lg:px-0 lg:text-xl truncate">
+            {originalLink}
           </p>
           <div className="h-[1px] bg-dark-gray opacity-25 lg:hidden"></div>
         </div>
         <div className="lg:flex lg:items-center lg:gap-6">
-          <p className="text-base font-medium leading-9 tracking-[0.12px] text-dark-cyan pt-[6px] pb-2 px-4 lg:pt-0 lg:pb-0 lg:px-0 lg:text-xl">
-            https://rel.ink/k4lKyk
+          <p className="text-base font-medium leading-9 tracking-[0.12px] text-dark-cyan pt-[6px] pb-2 px-4 lg:pt-0 lg:pb-0 lg:px-0 lg:text-xl truncate">
+            {shortLink}
           </p>
           <div className="px-4 pt-2 pb-4 lg:px-0 lg:pt-0 lg:pb-0 lg:w-[103px]">
             <Button
